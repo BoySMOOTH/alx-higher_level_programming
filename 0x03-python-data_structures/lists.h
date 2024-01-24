f53b84e230bde49101d673b128e2ad217e94e5b9
BoySMOOTH <smoothgadgets1@gmail.com>
2024-01-23 23:41:07 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,4 +21,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
+#endif
